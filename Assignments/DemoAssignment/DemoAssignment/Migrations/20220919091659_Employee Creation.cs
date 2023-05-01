fe15b37045ffa2313568c105289bea68319d734c
RitikaRaj29 <112872835+RitikaRaj29@users.noreply.github.com>
2023-05-01 17:34:22 +0530

    Add files via upload

---
+++ Assignments/DemoAssignment/DemoAssignment/Migrations/20220919091659_Employee Creation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoAssignment.Migrations
+{
+    public partial class EmployeeCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Assignments/DemoAssignment/DemoAssignment/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoAssignment.Models
+{
+    public class Employee
+    {
+       [Key]
+       public int id { get; set; }
+       public string EmpName { get; set; }
+       
+    }
+}
